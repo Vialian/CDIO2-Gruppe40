1729a8f482c18226c7887de1821a46ca1a5ac1e7
Alfkil Thorbjørn Wennermark <alfkil@gmail.com>
2019-11-15 21:37:51 +0100

    Project setup. Maven framework support added. Source files imported from previous project.

---
+++ Gruppe40/src/main/java/Entity/ChanceDeck.java
@@ -0,0 +1,97 @@
+public class ChanceDeck {
+    int index;
+    int[] deck;
+    public ChanceDeck() {
+        deck = new int[20];
+        mixDeck();
+    }
+
+    private mixDeck() {
+        int[] initDeck = new int[20];
+        for (int i = 0; i < 20; i++)
+            initDeck[i] = 1;
+        
+        int mixCount = 0;
+        while (mixCount < 20) {
+            int i;
+            do {
+                i = Math.random() * 20;
+            } while (initDeck[i] == 0);
+            deck[mixCount] = i;
+            initDeck[i] = 0;
+        }
+        index = 0;
+    }
+
+    public void drawCard() {
+        performAction(index++);
+        if (index == 20)
+            index = 0;
+    }
+    
+    public void performAction(int index) {
+        switch(index) {
+            case 0:
+                //Giv kort til bilen
+                //Tage et chancekort mere
+                //Til BIL:
+                //På næste tur skal du flytte til ledigt felt og køb det
+                //HVIS Ingen ledige: Køb af anden spiller
+            case 1:
+                //Ryk frem til start
+                //Modtag 2M
+            case 2:
+                //Ryk op til 5 felter frem
+            case 3:
+                //Ryk frem til et orange felt
+                //HVIS ledigt: Modtag felt gratis
+                //ELLERS betal leje til ejeren
+            case 4:
+                //ENTEN ryk 1 felt frem
+                //ELLER tag et chancekort mere
+            case 5:
+                //Giv dette kort til SKIBET
+                //Tag et chancekort mere
+                //Til SKIB:
+                //Næste tur, ryk til hvilket som helst ledigt felt og køb
+                //HVIS Ingen ledige: Køb af anden spiller
+            case 6:
+                //Du har spist for meget slik
+                //Betal 2M til banken
+            case 7:
+                //GRATIS FELT
+                //Ryk frem til Orange eller Grønt felt
+                //HVIS ledigt: Modtag felt
+                //ELLERS betal leje
+            case 8:
+                // --- || ----
+                // (men for lyseblåt felt)
+            case 9:
+                // Modtag kort
+                // Bruges til løsladelse fra fængsel
+            case 10:
+                // Ryk frem til Strandpromenaden
+            case 11:
+                // Som 5, men til KAT
+            case 12:
+                // Som 5 men til HUND
+            case 13:
+                // Fødselsdag: Modtag 1 M
+            case 14:
+                // som 7, men for pink eller Lyseblåt
+            case 15:
+                // Lektier: Modtag 2M
+            case 16:
+                // som 8, men for Rødt
+            case 17:
+                //Skaterparken: Ryk til Sk.
+                // HVIS ledigt: Modtag gratis
+                // ELLERS betal leje til ejer
+            case 18:
+                // Som 8, men for Lyseblåt eller Rødt
+            case 19:
+                // Som 8, men for Brunt eller Gult
+        }
+    }
+}
+
---
+++ Gruppe40/src/main/java/Entity/Player.java
@@ -0,0 +1,74 @@
+package Entity;
+
+import Entity.Die;
+
+public class Player {
+    private int points;
+    private String name;
+    private Die die1;
+    private Die die2;
+    private int currentTile;
+
+    public Player(String name, int initialPoints) {
+        die1 = new Die();
+        die2 = new Die();
+        this.points = initialPoints;
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int p) {
+        points = p;
+    }
+
+    public void addPoints(int p) {
+       setPoints(this.points + p);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int rollDice() {
+        int value1 = die1.roll();
+        int value2 = die2.roll();
+
+        return currentTile = value1 + value2 -1;
+    }
+
+    public int getCurrentTile() {
+        return currentTile;
+    }
+
+    public int getDieValue1() {
+        return die1.getFaceValue();
+    }
+
+    public int getDieValue2() {
+        return die2.getFaceValue();
+    }
+
+    /*public Boolean hasDoubles() {
+        return die1.getFaceValue() == die2.getFaceValue();
+    }*/
+
+    public Boolean hasWon() {
+        return points >= 3000 /*&& hasDoubleSix()*/;
+    }
+
+    public Boolean hasLost(){
+        return points < 0;
+    }
+
+    /*public Boolean hasDoubleOne() {
+        return hasDoubles() && die1.getFaceValue() == 1;
+    }
+
+    public Boolean hasDoubleSix() {
+        //didDouble6 = true;
+        return hasDoubles() && die1.getFaceValue() == 6;
+    }*/
+}
