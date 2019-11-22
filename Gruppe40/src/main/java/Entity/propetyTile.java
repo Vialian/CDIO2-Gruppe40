a0fc543e56ea81c6e03df7aadd53c68b88530d11
kenne <fifa2012@hotmail.com>
2019-11-22 11:47:15 +0100

    - Getter og setter sat for colour attribut - toString metode som returnere farven som ord -

--- Gruppe40/src/main/java/Entity/propetyTile.java
+++ Gruppe40/src/main/java/Entity/propetyTile.java
@@ -4,6 +4,8 @@ package Entity;
 
      private int cost;
      private int rent;
+     private int colour;
+
 
      public String getOwnedBy() {
          return ownedBy;
@@ -16,11 +18,20 @@ package Entity;
      private String ownedBy ="None";
      private String type = "propety";
 
-     public propetyTile(String name, String text, int cost, int rent) {
+     public propetyTile(String name, String text, int cost, int rent, int colour) {
          super(name, text);
          this.cost = cost;
          this.rent = rent;
+         this.colour = colour;
 
+     }
+
+     public void setColour(int colour){
+         this.colour = colour;
+     }
+
+     public int getColour(){
+         return colour;
      }
 
      @Override
@@ -33,5 +44,42 @@ package Entity;
      }
      public int getRent() {
          return rent;
+     }
+     public String colourString(int tilePosition){
+
+         // Note til mig selv: der skal ikke være "break" i switch case,
+         // fordi i dette tilfælde gør "return" det samme som "break"
+         // når der returneres string, så "afbrydes" eksekveringen af koden i switch case statementet
+         switch(this.colour)
+         {
+             case 1:
+                 return "Brown";
+
+             case 2:
+                 return "Light blue";
+
+             case 3:
+                 return "Purple";
+
+             case 4:
+                 return "Orange";
+
+             case 5:
+                 return "Red";
+
+             case 6:
+                 return "Yellow";
+
+             case 7:
+                 return "Green";
+
+             case 8:
+                 return "Blue";
+
+             default:
+                 return "No colour";
+         }
+
+
      }
  }
