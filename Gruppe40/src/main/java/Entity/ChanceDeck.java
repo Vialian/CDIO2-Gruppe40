114c1486c7bef6358de20964680ab04e9ad3d1d0
Adin1128 <Adin11281128@hotmail.com>
2019-11-26 10:10:55 +0100

    redid the whole chancedeck class.

--- Gruppe40/src/main/java/Entity/ChanceDeck.java
+++ Gruppe40/src/main/java/Entity/ChanceDeck.java
@@ -35,97 +35,165 @@ public class ChanceDeck {
     }
 
     public void drawCard(Player player, GUI gui, Board board, DiceGame game) {
-        performAction(index++, player, gui, board, game);
+        index++;
         index = index%20;
+        performAction(deck[index], player, gui, board, game);
     }
     
     private void performAction(int index, Player player, GUI gui, Board board, DiceGame game) {
         switch(index) {
+            int position;
+            String goTo;
             case 0:
                 gui.displayChanceCard("PROMISED LAND CARD: Bil");
-                drawCard(player, gui, board, game); //Tage et chancekort mere
                 Player bil = game.findPlayer(1);
-                bil.setPromisedRealEstate(true);
+                freeRealEstateCard.onDraw(bil);
+                drawCard(player, gui, board, game);
                 //Giv kort til bilen
                 //Til BIL:
                 //På næste tur skal du flytte til ledigt felt og køb det
                 //HVIS Ingen ledige: Køb af anden spiller
             case 5:
                 gui.displayChanceCard("PROMISED LAND CARD: Skib");
-                drawCard(player, gui, board, game); //Tage et chancekort mere
                 Player skib = game.findPlayer(2);
-                skib.setPromisedRealEstate(true);
+                freeRealEstateCard.onDraw(skib);
+                drawCard(player, gui, board, game);
                 // Samme, men for SKIBET
             case 11:
                 gui.displayChanceCard("PROMISED LAND CARD: Kat");
-                drawCard(player, gui, board, game); //Tage et chancekort mere
                 Player kat = game.findPlayer(3);
-                kat.setPromisedRealEstate(true);
+                freeRealEstateCard.onDraw(kat);
+                drawCard(player, gui, board, game);
                 // Som 5, men til KAT
             case 12:
                 gui.displayChanceCard("PROMISED LAND CARD: Hund");
-                drawCard(player, gui, board, game); //Tage et chancekort mere
                 Player hund = game.findPlayer(4);
-                hund.setPromisedRealEstate(true);
+                freeRealEstateCard.onDraw(hund);
+                drawCard(player, gui, board, game);
                 // Som 5 men til HUND
 
             case 3:
                 gui.displayChanceCard("FREE PROPERTY CARD: ORANGE");
-                int tileIndex = board.getNextTileColor("ORANGE");
-                moveBuyTile (index, player, board, gui, game);
+                goTo = gui.getUserSelection("You can choose between landing on either orange tile.","The skatepark","The swimmingpool");
+                position = 11;
+                if(goTo.equals("The skatepark")){
+                    position = 10;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 //Ryk frem til et orange felt
                 //HVIS ledigt: Modtag felt gratis
                 //ELLERS betal leje til ejeren
             case 7:
                 gui.displayChanceCard("FREE PROPERTY CARD: ORANGE/GRØN");
-                tileIndex = board.getNextTileColor2("ORANGE","GRØN");
-                moveBuyTile (index, player, board, gui, game);
+                goTo = gui.getUserSelection("You can choose between landing on either orange or green tiles.","The skatepark","The swimmingpool","The bowling alley","The zoo");
+                position = 11;
+                if(goTo.equals("The skatepark")){
+                    position = 10;
+                }
+                else if(goTo.equals("The bowling alley")){
+                    position = 19;
+                }
+                else if(goTo.equals("The zoo")){
+                    position = 20;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 //GRATIS FELT
                 //Ryk frem til ORANGE eller GRØNT felt
                 //HVIS ledigt: Modtag felt
                 //ELLERS betal leje
             case 8:
                 gui.displayChanceCard("FREE PROPERTY CARD: LYSEBLÅ");
-                tileIndex = board.getNextTileColor("LYSEBLÅ");
-                moveBuyTile (index, player, board, gui, game);
+                goTo = gui.getUserSelection("You can choose between landing on either orange tile.","The candyshop","The icre cream shop");
+                position = 5;
+                if(goTo.equals("The candyshop")){
+                    position = 4;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 // Samme, men for LYSEBLÅT felt
             case 14:
-                gui.displayChanceCard("FREE PROPERTY CARD: PINK/LYSEBLÅ");
-                tileIndex = board.getNextTileColor2("PINK", "LYSEBLÅ");
-                moveBuyTile (index, player, board, gui, game);
+                gui.displayChanceCard("FREE PROPERTY CARD: PINK/MØRKEBLÅ");
+                goTo = gui.getUserSelection("You can choose between landing on either orange or green tiles.","The museum","The library","The waterpark","The beach");
+                position = 7;
+                if(goTo.equals("The library")){
+                    position = 8;
+                }
+                else if(goTo.equals("The waterpark")){
+                    position = 22;
+                }
+                else if(goTo.equals("The beach")){
+                    position = 23;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 // som 7, men for pink eller Lyseblåt
             case 16:
                 gui.displayChanceCard("FREE PROPERTY CARD: RØD");
-                tileIndex = board.getNextTileColor("RØD");
-                moveBuyTile (index, player, board, gui, game);
+                goTo = gui.getUserSelection("You can choose between landing on either orange tile.","The casino","The cinema");
+                position = 13;
+                if(goTo.equals("The cinema")){
+                    position = 14;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 // som 8, men for Rødt
             case 17:
                 gui.displayChanceCard("FREE PROPERTY CARD: Skaterparken");
-                tileIndex = board.getNextTileName("Skaterparken");
-                moveBuyTile (index, player, board, gui, game);
+
+                sendToCard.setSendTo(10);
+                sendToCard.onDraw(player,gui,board,game,true);
                 //Skaterparken: Ryk til Sk.
                 // HVIS ledigt: Modtag gratis
                 // ELLERS betal leje til ejer
             case 18:
-                gui.displayChanceCard("FREE PROPERTY CARD: RØD");
-                tileIndex = board.getNextTileColor2("LYSEBLÅ", "RØD");
-                moveBuyTile (index, player, board, gui, game);
+                gui.displayChanceCard("FREE PROPERTY CARD: LYSEBLÅ/RØD");
+                goTo = gui.getUserSelection("You can choose between landing on either orange or green tiles.","The candyshop","The ice cream shop","The casino","The Cinema");
+                position = 5;
+                if(goTo.equals("The candyshop")){
+                    position = 4;
+                }
+                else if(goTo.equals("The casino")){
+                    position = 13;
+                }
+                else if(goTo.equals("The cinema")){
+                    position = 14;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 // Som 8, men for Lyseblåt eller Rødt
             case 19:
                 gui.displayChanceCard("FREE PROPERTY CARD: BRUN/GUL");
-                tileIndex = board.getNextTileColor2("BRUN", "GUL");
-                moveBuyTile (index, player, board, gui, game);
+                goTo = gui.getUserSelection("You can choose between landing on either orange or green tiles.","The burgerbar","The pizzeria","The toy store","The pet store");
+                position = 1;
+                if(goTo.equals("The pizzeria")){
+                    position = 2;
+                }
+                else if(goTo.equals("The toy store")){
+                    position = 16;
+                }
+                else if(goTo.equals("The pet store")){
+                    position = 17;
+                }
+                sendToCard.setSendTo(position);
+                sendToCard.onDraw(player,gui,board,game,true);
                 // Som 8, men for Brunt eller Gult
 
             case 1:
                 gui.displayChanceCard("GO TO START CARD");
                 sendToCard.setSendTo(0);
-                sendToCard.onDraw(player);
+                sendToCard.onDraw(player,false);
+                moneyCard.setMoney(2);
+                moneyCard.onDraw(player);
                 //Ryk frem til start
                 //Modtag 2M
             case 2:
                 gui.displayChanceCard("MOVE FORWARD CARD");
                 int move = gui.getUserInteger("Hvor mange felter vil du flytte frem (1-5)?");
+                while(!(0<move && move<6)){
+                    move = gui.getUserInteger("Hvor mange felter vil du flytte frem (1-5)?");
+                }
                 moveForwardCard.setMove(move);
                 moveForwardCard.onDraw(player);
 
@@ -151,18 +219,18 @@ public class ChanceDeck {
 
             case 9:
                 gui.displayChanceCard("GET OUT OF JAIL FREE CARD");
-                player.incrementReleaseCards();
+                getOutOfJailFreeCard.onDraw(player);
                 // LØSLADELSESKORT
             case 10:
                 gui.displayChanceCard("Strandpromenaden CARD");
-                tileIndex = board.getNextTileName("Strandpromenaden");
-                sendToCard.setSendTo(index);
-                sendToCard.onDraw(player);
-                String jaNej = gui.getUserSelection("Har du fødselsdag i dag?", "Ja", "Nej");
-                if(jaNej.equals("Ja")) {
-                    moneyCard.setMoney(1);
-                    moneyCard.onDraw(player);
-                }
+                sendToCard.setSendTo(23);
+                sendToCard.onDraw(player,false);
+            case 13:
+                gui.displayChanceCard("BIRTHDAY CARD");
+                gui.showMessage("Det er din fødselsdag. Modtag 1M.");
+                moneyCard.setMoney(1);
+                moneyCard.onDraw(player);
+
                 // Ryk frem til Strandpromenaden
                 // Fødselsdag: Modtag 1 M
             case 15:
