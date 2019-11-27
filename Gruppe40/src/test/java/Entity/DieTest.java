08e3e2712865fb5e4f0a687d1daf79f66578e5c2
Vialian <dotasoren@gmail.com>
2019-11-27 15:10:29 +0100

    DieTest

--- Gruppe40/src/test/java/Entity/DieTest.java
+++ Gruppe40/src/test/java/Entity/DieTest.java
@@ -11,7 +11,7 @@ public class DieTest {
         int amountTest =  1000;
         for (int i = 0; i < amountTest; i++) {
             faceValue = (int)(Math.random() * 6.0 + 1.0);
-            assertEquals(1,faceValue,6);
+            assertEquals(3.5, faceValue,2.5);
         }
 
 
