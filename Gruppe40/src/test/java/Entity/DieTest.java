7225e02ea4c78c07ad336a7f47158d678976ec8e
Vialian <dotasoren@gmail.com>
2019-11-27 14:55:28 +0100

    DieTest

---
+++ Gruppe40/src/test/java/Entity/DieTest.java
@@ -0,0 +1,17 @@
+package Entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DieTest {
+
+    @Test
+    public void roll() {
+        
+    }
+
+    @Test
+    public void getFaceValue() {
+    }
+}
