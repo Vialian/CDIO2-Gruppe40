54552c78e8b79d0d96d1cdedfd629a79fc31ed25
Vialian <dotasoren@gmail.com>
2019-11-27 15:18:29 +0100

    DieTest

--- Gruppe40/src/test/java/Entity/DieTest.java
+++ Gruppe40/src/test/java/Entity/DieTest.java
@@ -6,12 +6,19 @@ import static org.junit.Assert.*;
 
 public class DieTest {
     private int faceValue;
+    private int prob = (1/6);
+    int[] amountCount = new int[7];
     @Test
     public void roll() {
-        int amountTest =  1000;
-        for (int i = 0; i < amountTest; i++) {
+        int amountTest =  1001;
+        for (int i = 1; i < amountTest; i++) {
             faceValue = (int)(Math.random() * 6.0 + 1.0);
             assertEquals(3.5, faceValue,2.5);
+            amountCount[faceValue] += 1;
+        }
+
+        for (int i = 1; i < 6  ; i++) {
+            assertEquals(prob, (amountCount[i]/amountTest), 0.01);
         }
 
 
@@ -19,6 +26,9 @@ public class DieTest {
 
     @Test
     public void getFaceValue() {
-        assertEquals(1,faceValue,6);
+
+        for (int i = 1; i < 6; i++) {
+
+        }assertEquals(1,faceValue,6);
     }
 }
