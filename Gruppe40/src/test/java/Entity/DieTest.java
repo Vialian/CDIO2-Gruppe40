4fbfec10c53bac65ba1be78565b3d8746a40fe71
Vialian <dotasoren@gmail.com>
2019-11-27 15:01:51 +0100

    DieTest

--- Gruppe40/src/test/java/Entity/DieTest.java
+++ Gruppe40/src/test/java/Entity/DieTest.java
@@ -5,13 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DieTest {
-
+    private int faceValue;
     @Test
     public void roll() {
-        
+        faceValue = (int)(Math.random() * 6.0 + 1.0);
+
     }
 
     @Test
     public void getFaceValue() {
+        assertEquals(1,faceValue,6);
     }
 }
