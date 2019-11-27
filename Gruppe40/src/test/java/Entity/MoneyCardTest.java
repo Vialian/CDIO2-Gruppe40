4322cf225d13cac0ac3ef37884b11406e9bed5a6
Adin1128 <Adin11281128@hotmail.com>
2019-11-27 15:11:17 +0100

    Added moneyCardTest

---
+++ Gruppe40/src/test/java/Entity/MoneyCardTest.java
@@ -0,0 +1,21 @@
+package Entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoneyCardTest {
+
+    @Test
+    public void setMoney() {
+
+    }
+
+    @Test
+    public void getMoney() {
+    }
+
+    @Test
+    public void onDraw() {
+    }
+}
