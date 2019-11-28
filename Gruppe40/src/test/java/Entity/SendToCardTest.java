8f894fa7e38c4b8547cd285dc0d555c052421b3b
Alexander Lambrecht <alexanderlambrecht@gmail.com>
2019-11-28 13:25:35 +0100

    sendToCard

--- Gruppe40/src/test/java/Entity/SendToCardTest.java
+++ Gruppe40/src/test/java/Entity/SendToCardTest.java
@@ -1,5 +1,7 @@
 package Entity;
 
+import Control.DiceGame;
+import gui_main.GUI;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -16,5 +18,38 @@ public class SendToCardTest {
 
     @Test
     public void onDraw() {
+        SendToCard card = new SendToCard();
+
+        int testSendTo = 4;
+        //check for grund er gratis og ikke gratis
+        card.setSendTo(testSendTo);
+        GUI gui = new GUI();
+        DiceGame game = new DiceGame(gui,5);
+        Board board = game.getBoard();
+        Player pl1 = game.findPlayer(1);
+        int testStartM = pl1.getMoney();
+
+        card.onDraw(pl1,gui,board,game, false);
+        PropertyTile tile =(PropertyTile) board.getTile(testSendTo);
+        int tileCost = tile.getCost();
+
+        assertEquals(testStartM- tileCost, pl1.getMoney());
+        int[] expected = {testSendTo};
+        assertArrayEquals(expected,pl1.getOwnedProperties());
+
+        //hvis det er gratis
+        Player pl2 = game.findPlayer(2);
+        testSendTo = 10;
+
+        pl2.setMoney(testStartM);
+        card.setSendTo(testSendTo);
+
+
+        card.onDraw(pl2,gui,board,game, true);
+        assertEquals(testStartM, pl2.getMoney());
+        int[] expected2 = {testSendTo};
+        assertArrayEquals(expected2, pl2.getOwnedProperties());
+
+
     }
 }
