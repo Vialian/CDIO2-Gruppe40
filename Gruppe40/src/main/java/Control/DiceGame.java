a95aebb7fec6a7507e25a7c248f735e38c975254
Adin1128 <Adin11281128@hotmail.com>
2019-11-28 11:38:07 +0100

    Fixed ColourPair

--- Gruppe40/src/main/java/Control/DiceGame.java
+++ Gruppe40/src/main/java/Control/DiceGame.java
@@ -662,7 +662,7 @@ public class DiceGame {
                 // Kontrol om næste tile er propertyTile eller ej
                 // Hvis tile ikke er en instans af propertyTile, så forsættes eksekvering af koden
                 System.out.println(tile instanceof UselessTile);
-                if (!(tile instanceof PropertyTile)) {
+                if (!(previousProperty instanceof PropertyTile)) {
                     continue;
                 }
 
